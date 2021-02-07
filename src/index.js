d114297948bf090c9df5c46a7dd39b5b2ee85376
makssen <maks.sen.1903@mail.ru>
2021-02-07 20:39:48 +0300

    feat: add method for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let lengthOfStr = str.length;
+
+    for (let i = 0; i < lengthOfStr; i++) {
+        for (let item of bracketsConfig) {
+            if (str.includes(item.join(''))) {
+                str = str.replace(item.join(''), '');
+            }
+        }
+        lengthOfStr--;
+    }
+    return !str.length
 }
